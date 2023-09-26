8bb371df1263cfa18ef57c02e8e74b5b92fdc6e9
Santiago Cocca <s.cocca.u@gmail.com>
2023-09-25 22:33:30 -0300

    Se agregan comentarios

--- calculadoraGuia.py
+++ calculadoraGuia.py
@@ -1,39 +1,78 @@
+#Crear la variable numbers que sera un arreglo o lista vacia
 numbers=[]
 
+#creamos una funcion llamada basiCalculator que no recibe parametros
 def basicCalculator():
+    #Iniciamos un bucle infinito
     while True:
+        #Mostramos un mensaje por pantalla
         print("Si desea salir no ingrese un valor y presione enter")
+        #Solicitamos una entrada por parte del usuario
         operation=input("Ingrese la operacion matematica que desea realizar: ")
+        #Verificamos si la entrada esta vacia
         if not operation:
+            #Si la entrada esta vacia entramos en el condicional if
+            #y causamos el fin del bucle infinito
             break
+        #Si la entrada no esta vacia con try-except veremos si se puede ejecutar un bloque de codigo
         try:
+            #Bloque de codigo a evaluar. Declaramos la variable result la cual tomara el valor de eval(operatior)
+            #el metodo eval(operation), recibira un string o cadena de caracteres y evaluara si es ejecutable como codigo python
             result=eval(operation)
+            #Si se pudo ejecutar mostraremos el resultado por pantalla
             print(result)
+        #En caso de no poder poder ejecutar el bloque de codigo que se encuentra dentro de try, pasamos a except
         except:
+            #mostramos un mensaje de error
             print("Error: su operacion no se pudo realizar")
 
+#Dado que para algunas funciones necesitaremos un arreglo, modificaremos el arreglo que declaramos vacio al inicio
+#mediante una funcion nueva llamada crearArray que no recibe parametros , ya que seran solicitados en la misma funcion
 def crearArray():
+    #con numbers.clear() limpiamos nuestro arreglo o lista en caso de que este tenga informacion almacenada
     numbers.clear()
+    #inciamos un bucle infinito
     while True:
+        #Solicitamos una entrada al usuario
         number=input("Ingrese un numero entero a agregar a su array")
+        #mostramos por pantalla la opcion para terminar el bucle infinito
         print("Si no desea agregar mas numeros ingrese t")
+        #Verificamos que la opcion no sea la de termino del bucle antes de comenzar la operacion para agregar el valor al arreglo o lista
         if number=="t":
-            print(numbers)
+            #Con break finalizamos el bucle infinito
             break
-        numbers.append(int(number))
-        print(numbers)
-        
+        #Con try-except veremos si el codigo se puede ejecutar
+        try:
+            #Si el valor ingresado por el usuario puede ser transformado a tipo int (Integer) se agrega al array
+            numbers.append(int(number))
+        #Si el bloque de codigo dentro de try no se puede ejecutar, ingresamos al bloque contenido dentro de except
+        except:
+            #Mostramos un mensaje de error por pantalla
+            print("El valor ingresado no es numero")
+
+#Creamos una nueva funcion llamada addMultipleNumbers y esta recibe como parametro el arreglo o lista que declaramos
+# al inicio del codigo        
 def addMultipleNumbers(numbers):
+    #verificamos que la lista o arreglo contenga informacion, en caso de estar vacia ejecutara la funcion crearArray()
     if len(numbers)==0:
+        #llamo a la funcion
         crearArray()
+    #En caso de que el arreglo no este vacio, se ejecutara el siguiente bloque de codigo
     else:
+        #creo e inicializo una variable donde almacenare el resultado de la suma
         result=0
+        #Mediante un ciclo for recorremos el arreglo, mientras i tenga alguna posicion en el arreglo, si llega al final de este finaliza
         for i in numbers:
+            #En cada iteracion del ciclo for adicionaremos a result, el valor de la posicion del indice
             result+=i
+        #muestro el resultado por pantalla
         print(result)
 
+#creo una nueva funcion llamada multiplyMultipleNumbers que recibe como parametro un arreglo
 def multiplyMultipleNumbers(numbers):
+    #Verificamos si el arreglo esta vacio
     if len(numbers)==0:
+        #Si esta vacio llamo a la funcion crearArray() para 
         crearArray()
     else:
         result=1
